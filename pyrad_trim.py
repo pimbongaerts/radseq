87d7a184cd8bb473d62c1d899eb57c05f66bd79d
Pim Bongaerts <pim@uq.edu.au>
2017-01-30 10:36:18 +1000

    Improved pyrad_trim.py script

--- pyrad_trim.py
+++ pyrad_trim.py
@@ -9,33 +9,41 @@ __author__ = 'Pim Bongaerts'
 __copyright__ = 'Copyright (C) 2016 Pim Bongaerts'
 __license__ = 'GPL'
 
+def determine_seq_start(line):
+    """ Determine the start position of the sequence """
+    sample, seq = line.split()
+    seq_start = line.find(seq)
+    if seq_start < 0:
+        sys.exit('Unexpected error')
+    else:
+        return seq_start
 
 def main(pyrad_filename, seq_length):
     # Open input file
     pyrad_file = open(pyrad_filename, 'r')
     # Iterate through file and trim sequences
-    output_line_length = 0
+    seq_start = 0
     for line in pyrad_file:
         if line[0] == '/':
             # Extract locus info and number
-            cols = line.split('|')
+            cols = line.strip().split('|')
             locus_info = cols[0]
-            locus_nr = cols[1].strip()
+            locus_nr = cols[1]
             # Trim locus info
-            locus_info_trim = locus_info[0:output_line_length + 1]
+            locus_info_trim = locus_info[0:output_line_length]
             print('{0}|{1}'.format(locus_info_trim, locus_nr))
         else:
+            # Determine delimiter if not yet set (first line)
+            if seq_start == 0:
+                seq_start = determine_seq_start(line)
             # Extract sample name and sequence
-            cols = line.split()
-            sample = cols[0]
-            seq = cols[1].strip()
+            sample, seq = line.strip().split()
             # Trim sequence and determine new line length
-            trim_seq = cols[1][0:int(seq_length)]
+            trim_seq = seq[0:int(seq_length)]
             # Output trimmed sequence
-            output_line = '{0}\t{1}'.format(sample, trim_seq)
+            output_line = '{0}{1}'.format(sample.ljust(seq_start), trim_seq)
             print(output_line)
             output_line_length = len(output_line)
-
     pyrad_file.close()
 
 if __name__ == '__main__':
